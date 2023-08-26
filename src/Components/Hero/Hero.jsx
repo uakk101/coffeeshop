097dd5415870cb7de82221fcca9d26c4712bf9e9
Eric <ericyounas32@gmail.com>
2023-08-26 02:51:48 -0700

    added hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import './Hero.css'
+import hero from '../../images/hero.png'
 const Hero = () => {
   return (
-    <div>Hero</div>
+
+    <div className='Hero'>
+      <div className="HeroContainer">
+
+
+        <div className='HeroText'>
+          <span>Alowishus</span><br />
+          <span>Delicious</span><br />
+          <span>Coffee</span>
+
+          <p>A drink from the 'My Alowishus' bottled brews<br />range OR grab one of our delicious coffee's.</p>
+
+          <div className='Buttons'>
+            <button className='DownloadBtn'>Download App</button >
+            <button className='CoffeeBtn'>Shop Coffee</button >
+          </div>
+        </div>
+        <div className="image-container">
+          <img src={hero} alt="Logo" className="rotated-image" />
+        </div>
+      </div>
+    </div>
   )
 }
 
